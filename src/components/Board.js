ec851c9f873e286acd09514e9742a249320b24ad
Troy Mathis <troy.mathis00@gmail.com>
2022-12-22 16:39:47 -0600

    board modified

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,43 @@
 const Board = () => {
-  <div id="board">
-    <div id="row">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <div id="row">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <div id="row">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <div id="row">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <div id="row">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  </div>;
+  return (
+    <table>
+      <tr id="row-1">
+        <td id="sqr-1"></td>
+        <td id="sqr-2"></td>
+        <td id="sqr-3"></td>
+        <td id="sqr-4"></td>
+        <td id="sqr-5"></td>
+      </tr>
+      <tr id="row-2">
+        <td id="sqr-6"></td>
+        <td id="sqr-7"></td>
+        <td id="sqr-8"></td>
+        <td id="sqr-9"></td>
+        <td id="sqr-10"></td>
+      </tr>
+      <tr id="row-3">
+        <td id="sqr-11"></td>
+        <td id="sqr-12"></td>
+        <td id="sqr-13"></td>
+        <td id="sqr-14"></td>
+        <td id="sqr-15"></td>
+      </tr>
+      <tr id="row-4">
+        <td id="sqr-16"></td>
+        <td id="sqr-17"></td>
+        <td id="sqr-18"></td>
+        <td id="sqr-19"></td>
+        <td id="sqr-20"></td>
+      </tr>
+      <tr id="row-5">
+        <td id="sqr-21"></td>
+        <td id="sqr-22"></td>
+        <td id="sqr-23"></td>
+        <td id="sqr-24"></td>
+        <td id="sqr-25"></td>
+      </tr>
+    </table>
+  );
 };
 
 export default Board;
